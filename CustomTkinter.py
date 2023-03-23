5c9fb2f0849310c80be360b88776d0efb455d842
Nasahahapatimal <dusan.blagojevic1990@gmail.com>
2023-03-23 23:19:06 +0100

    Added .py files and .txt file

---
+++ CustomTkinter.py
@@ -0,0 +1,226 @@
+from tkinter import *
+import customtkinter
+from FInalWork import *
+from PIL import ImageGrab
+
+
+customtkinter.set_appearance_mode("default")
+
+root = customtkinter.CTk()
+root.title("Kuhinja")
+root.geometry("450x600")
+bold_font = ("TkDefaultFont", 11, "bold")
+bold_font2 = ("TkDefaultFont", 9, "bold")
+
+options = []
+for x in a.recepti:
+    options.append(x["recept"])
+
+l_0 = customtkinter.CTkLabel(root, text="PONEDELJAK", font=bold_font).grid(
+    row=0, column=0, padx=0, pady=5)
+
+l_1 = customtkinter.CTkLabel(root, text="DORUCAK", font=bold_font2).grid(
+    row=1, column=0, padx=2, pady=2)
+l_2 = customtkinter.CTkLabel(root, text="---")
+l_2.grid(row=3, column=0, padx=2, pady=2)
+selected_option = StringVar(root)
+selected_option.set("")
+option_menu = OptionMenu(root, selected_option, *options)
+option_menu.grid(row=3, column=1, padx=15, pady=2)
+b = customtkinter.CTkButton(root, text="Odaberi dorucak", command=lambda: [(
+    l_2.configure(text=a.odaberi_dorucak(dorucak=selected_option.get()))), selected_option.set("Bon Apetit :)")])
+b.grid(row=4, column=0, padx=2, pady=2)
+
+
+l_3 = customtkinter.CTkLabel(root, text="RUCAK", font=bold_font2).grid(
+    row=5, column=0, padx=2, pady=25)
+l_4 = customtkinter.CTkLabel(root, text="---")
+l_4.grid(row=6, column=0, padx=2, pady=2)
+b_1 = customtkinter.CTkButton(root, text="Odaberi rucak", command=lambda: [(
+    l_4.configure(text=a.odaberi_rucak(rucak=selected_option1.get()))), selected_option1.set("Bon Apetit :)")])
+b_1.grid(row=7, column=0, padx=2, pady=2)
+selected_option1 = StringVar(root)
+selected_option1.set("")
+option_menu = OptionMenu(root, selected_option1, *options)
+option_menu.grid(row=6, column=1, padx=15, pady=2)
+
+l_5 = customtkinter.CTkLabel(root, text="VECERA", font=bold_font2).grid(
+    row=8, column=0, padx=2, pady=25)
+l_6 = customtkinter.CTkLabel(root, text="---")
+l_6.grid(row=9, column=0, padx=2, pady=2)
+b_2 = customtkinter.CTkButton(root, text="Odaberi veceru", command=lambda: [(
+    l_6.configure(text=a.odaberi_veceru(vecera=selected_option2.get()))), selected_option2.set("Bon Apetit :)")])
+b_2.grid(row=10, column=0, padx=2, pady=2)
+selected_option2 = StringVar(root)
+selected_option2.set("")
+option_menu = OptionMenu(root, selected_option2, *options)
+option_menu.grid(row=9, column=1, padx=15, pady=2)
+
+
+def Dodaj_recept():
+    t = Toplevel(root)
+
+    t.geometry("300x600")
+
+    def add_entry():
+        l3_t = customtkinter.CTkLabel(t, text="Ime sastojka")
+        l3_t.pack()
+        new_e1_t = customtkinter.CTkEntry(t, placeholder_text="")
+        new_e1_t.pack()
+
+        l4_t = customtkinter.CTkLabel(t, text="Kolicina")
+        l4_t.pack()
+        new_e2_t = customtkinter.CTkEntry(t, placeholder_text="")
+        new_e2_t.pack()
+
+        entries.append((new_e1_t, new_e2_t))
+
+    def get_kwargs():
+        kwargs = {}
+        for entry_pair in entries:
+            key = entry_pair[0].get()
+            value = entry_pair[1].get()
+            if key and value:
+                kwargs[key] = value
+        return kwargs
+
+    b = customtkinter.CTkButton(t, text="Dodaj recept", command=lambda: a.dodaj_recept(
+        recept=e_t.get(), **get_kwargs()))
+    b.pack()
+
+    add_entry_button = customtkinter.CTkButton(
+        t, text="Dodaj novi sastojak", command=add_entry)
+    add_entry_button.pack(pady=10)
+
+    l1 = customtkinter.CTkLabel(t, text="")
+    l1.pack()
+
+    l_t = customtkinter.CTkLabel(t, text="Recept")
+    l_t.pack()
+
+    e_t = customtkinter.CTkEntry(t, placeholder_text="")
+    e_t.pack()
+
+    l1_t = customtkinter.CTkLabel(t, text="Ime sastojka")
+    l1_t.pack()
+
+    e1_t = customtkinter.CTkEntry(t, placeholder_text="")
+    e1_t.pack()
+
+    l2_t = customtkinter.CTkLabel(t, text="Kolicina")
+    l2_t.pack()
+
+    e2_t = customtkinter.CTkEntry(t, placeholder_text="")
+    e2_t.pack()
+
+    entries = [(e1_t, e2_t)]
+
+
+def dodaj_zacin():
+
+    t1 = Toplevel(root)
+    t1.geometry("300x450")
+
+    l_1 = customtkinter.CTkLabel(t1, text="Naziv namirnice").pack()
+
+    e_1 = customtkinter.CTkEntry(t1, placeholder_text="")
+    e_1.pack()
+
+    l_2 = customtkinter.CTkLabel(t1, text="kolicina").pack()
+
+    e_2 = customtkinter.CTkEntry(t1, placeholder_text="")
+    e_2.pack()
+
+    l_3 = customtkinter.CTkLabel(t1, text="proteini").pack()
+
+    e_3 = customtkinter.CTkEntry(t1, placeholder_text="")
+    e_3.pack()
+
+    l_4 = customtkinter.CTkLabel(t1, text="hidrati").pack()
+
+    e_4 = customtkinter.CTkEntry(t1, placeholder_text="")
+    e_4.pack()
+
+    l_5 = customtkinter.CTkLabel(t1, text="masti").pack()
+
+    e_5 = customtkinter.CTkEntry(t1, placeholder_text="")
+    e_5.pack()
+
+    l_6 = customtkinter.CTkLabel(t1, text="kalorije").pack()
+
+    e_6 = customtkinter.CTkEntry(t1, placeholder_text="")
+    e_6.pack()
+
+    b_1 = customtkinter.CTkButton(t1, text="Dodaj zacin",  command=lambda: [l_7.configure(
+        text=a.dodaj_zacin(e_1.get(), e_2.get(), e_3.get(), e_4.get(), e_5.get(), e_6.get()))])
+    b_1.pack(pady=7)
+
+    l_7 = customtkinter.CTkLabel(t1, text="")
+    l_7.pack()
+
+
+def obrisi_recept():
+    t2 = Toplevel(root)
+    t2.geometry("300x450")
+
+    options = []
+    for x in a.recepti:
+        options.append(x["recept"])
+
+    l_t2 = customtkinter.CTkLabel(t2, text="")
+    l_t2.pack()
+
+    selected_option = StringVar(t2)
+    selected_option.set("")
+    option_menu = OptionMenu(t2, selected_option, *options)
+    option_menu.pack()
+
+    b_t2 = customtkinter.CTkButton(t2, text="Obrisi recept", command=lambda: l_t2.configure(
+        text=a.obrisi_recept(jelo=selected_option.get())))
+    b_t2.pack()
+
+
+menubar = Menu(master=root)
+
+accountmenu = Menu(menubar, tearoff=0)
+middlemenu = Menu(menubar, tearoff=0)
+exitmenu = Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="Admin", menu=accountmenu)
+accountmenu.add_command(label="Dodaj recept", command=lambda: Dodaj_recept())
+accountmenu.add_command(label="Dodaj zacin", command=lambda: dodaj_zacin())
+accountmenu.add_command(label="Obrisi recept", command=lambda: obrisi_recept())
+
+
+menubar.add_command(label="Napravi spisak", command=lambda: [(a.spisak_za_kupovinu(a.odaberi_dorucak(l_2.cget("text")))),
+                                                             (a.spisak_za_kupovinu(
+                                                                 a.odaberi_rucak(l_4.cget("text")))),
+                                                             (a.spisak_za_kupovinu(a.odaberi_veceru(l_6.cget("text")))), (a.stampaj())])
+
+
+menubar.add_cascade(label="Exit", menu=exitmenu)
+exitmenu.add_command(label="Exit", command=root.destroy)
+
+root.configure(menu=menubar)
+
+
+b_1 = Button(root, text="Print Screenshot")
+
+
+def print_screenshot():
+
+    x = root.winfo_rootx()
+    y = root.winfo_rooty()
+    w = root.winfo_width()
+    h = root.winfo_height()
+
+    screenshot = ImageGrab.grab(bbox=(x, y, x+w, y+h))
+
+    screenshot.show()
+    screenshot.save("screenshot.png")
+
+
+b_1.config(command=print_screenshot)
+b_1.grid(row=10, column=10, padx=2, pady=2)
+
+root.mainloop()
---
+++ FInalWork.py
@@ -0,0 +1,205 @@
+import json
+
+
+class Namirnica:
+
+    def __init__(self, namirnica, kolicina, proteini, hidrati, masti, kalorije):
+
+        self.namirnica = namirnica
+        self.kolicina = kolicina
+        self.proteini = proteini
+        self.hidrati = hidrati
+        self.masti = masti
+        self.kalorije = kalorije
+
+    def __str__(self):
+
+        return("{}|{}|{}|{}|{}|{}".format(self.namirnica, self.kolicina, self.proteini, self.hidrati, self.masti, self.kalorije))
+
+
+class Kuhinja:
+
+    def __init__(self):
+
+        self.namirnice = []
+        self.recepti = []
+        self.output = {}
+
+    def ucitaj_namirnice(self):
+
+        x = [l.strip() for l in open("kalorije.txt")]
+
+        for nm in x:
+
+            namirnica = nm.split("|")[0]
+            kolicina = nm.split("|")[1]
+            proteini = nm.split("|")[2]
+            hidrati = nm.split("|")[3]
+            masti = nm.split("|")[4]
+            kalorije = nm.split("|")[5]
+
+            proizvod = Namirnica(namirnica, kolicina,
+                                 proteini, hidrati, masti, kalorije)
+            self.namirnice.append(proizvod)
+
+    def ucitaj_recepte(self):
+
+        with open("recepti.txt", "r") as f:
+            # Initialize an empty list to store the dictionaries
+            # Read each line of the file
+            for line in f:
+                # Load the dictionary from the line and append it to the list
+                recipe = json.loads(line.strip())
+                self.recepti.append(recipe)
+
+    def ocitaj_kalorije(self, nammm):
+
+        for nm in self.namirnice:
+            if nammm.lower() == nm.namirnica.lower():
+                return(nm.kalorije)
+        return ("Ne postoji uneta namirnica")
+
+    def kalorijska_vrednost(self, naziv_recepta):
+
+        kalorije = 0
+
+        for x in self.recepti:
+            if naziv_recepta.lower() == x["recept"].lower():
+                for y in self.namirnice:
+                    if y.namirnica.lower() in x:
+                        if y.kolicina == "komad":
+                            kalorije = kalorije + \
+                                float(x[y.namirnica.lower()])*float(y.kalorije)
+                        else:
+                            kalorije = kalorije + \
+                                float(x[y.namirnica.lower()]) / \
+                                100*(float(y.kalorije))
+
+        if kalorije > 0:
+            return ("Ukupna vrednost kalorija za odabrani recept {} je {}".format(naziv_recepta, kalorije))
+        return ("Ne postoji uneti recept!")
+
+    def dodaj_recept(self, **kwargs):
+
+        result = {**kwargs}
+        result = {k: v for k, v in result.items() if v != "" or k != ""}
+
+        # dodaj ""edge case"" ako je recept = "" ili sastojan "str"
+
+        for recept in self.recepti:
+            if (kwargs["recept"]) == recept["recept"]:
+                return ("Već postoji uneti recept")
+
+        if result == {} or "recept" not in result or len(result) == 1:
+            return
+        else:
+            self.recepti.append(result)
+            self.update_file_recepti()
+            return ("Uspesno dodat novi recept!")
+
+    def obrisi_recept(self, jelo=""):
+        for recept in self.recepti:
+            if jelo.lower() == recept["recept"].lower():
+                self.recepti.remove(recept)
+                self.update_file_recepti()
+                return("Uspesno ste uklonili recept {}".format(recept["recept"]))
+        return("Ne postoji odabrani recept sa tim nazivom")
+
+    def update_file_recepti(self):
+        with open("recepti.txt", "w") as f:
+            for recept in self.recepti:
+                json_str = json.dumps(recept)
+                f.write(json_str)
+                f.write('\n')
+
+    def odaberi_dorucak(self, dorucak=""):
+
+        for recept in self.recepti:
+
+            if dorucak.lower() == recept["recept"].lower():
+                return(dorucak)
+        return ("Ne postoji odabrani dorucak")
+
+    def odaberi_rucak(self, rucak=""):
+
+        for recept in self.recepti:
+
+            if rucak.lower() == recept["recept"].lower():
+                return(rucak)
+        return ("Ne postoji odabrani rucak")
+
+    def odaberi_veceru(self, vecera=""):
+
+        for recept in self.recepti:
+
+            if vecera.lower() == recept["recept"].lower():
+                return(vecera)
+        return ("Ne postoji odabrana vecera")
+
+    def odaberi_random_jelo(self, dorucak="", rucak="", vecera=""):
+        pass
+
+    def ispis_recepta_dan(self):
+        pass
+
+    def spisak_za_kupovinu(self, *args):
+
+        #output = {}
+        for x in args:
+            for recept in self.recepti:
+                if x.lower() == recept["recept"].lower():
+                    for key, value in recept.items():
+                        if key != "recept":
+                            if key not in self.output:
+                                self.output[key] = 0
+                            self.output[key] += int(value)
+
+    def stampaj(self):
+
+        f = open("Spisak_za_kupovinu.txt", "w")
+        f.close()
+
+        f = open("Spisak_za_kupovinu.txt", "a")
+        for key, value in self.output.items():
+            print(("{}:{}\n".format(key, value)), file=f)
+        f.close()
+
+        self.output = {}
+
+    def report_fali_sastojak(self):
+        pass
+
+    def update_file_zacin(self):
+
+        f = open("kalorije.txt", "w")
+        f.close()
+
+        f = open("kalorije.txt", "a")
+        for namirnica in self.namirnice:
+            print(namirnica, file=f)
+        f.close()
+
+    def dodaj_zacin(self, namirnica, kolicina, proteini, hidrati, masti, kalorije):
+
+        for x in self.namirnice:
+            if namirnica.lower() == x.namirnica.lower():
+                return ("Namirnica već postoji")
+
+        if "" in [namirnica, kolicina, proteini, hidrati, masti, kalorije]:
+            return ("Morate popuniti polja ispravno")
+
+        nam = Namirnica(namirnica.capitalize(), kolicina,
+                        proteini, hidrati, masti, kalorije)
+        self.namirnice.append(nam)
+        self.update_file_zacin()
+        return ("Uspeno ste uneli zacin {}".format(namirnica))
+
+
+a = Kuhinja()
+a.ucitaj_namirnice()
+a.ucitaj_recepte()
+print(a.spisak_za_kupovinu("Lovacke snicle",
+      "Rolat sarma", "Lovacke snicle", "Lovacke snicle", "Lovacke snicle"))
+a.stampaj()
+# for x, y in a.output.items():
+#print("{}: {}".format(x, y))
